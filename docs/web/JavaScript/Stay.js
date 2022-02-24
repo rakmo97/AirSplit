78544df449439cf9107de502027caff457550c88
K-Ramos <kevinramos.engineer@gmail.com>
2022-02-23 20:18:02 -0600

    testing docs folder

---
+++ docs/web/JavaScript/Stay.js
@@ -0,0 +1,213 @@
+class Stay
+{
+    constructor(total_price, num_nights)
+    {
+
+        this.total_price = total_price;
+        this.num_nights = num_nights;
+        this.name_list = []; 
+        this.nights_staying_list = [];
+
+        // Total Cost each night
+        this.per_night_total_cost = total_price/num_nights;
+
+        // Flag for if original cost was calculated
+        this.original_costs_calculated = false;
+        this.num_guests = 0;
+
+    }
+
+    AddPerson(name, nights_staying)
+    {
+        // Check if person is already in the name list. We don't want duplicate names
+        if(this.name_list.includes(name))
+        {            
+            // Figure out how to raise warnings/errors in JS if this is needed
+            // raise AttributeError("Person {} already added to list. Change name to distinguish.".format(name))
+        }
+
+        // Check if person is staying after checkout.  Can't do this!
+        if(!nights_staying.every(nights_staying => nights_staying < this.num_nights))
+        {
+            // raise AttributeError("Person {} cannot stay after the {}th night.".format(name, this.num_nights))
+        }
+
+        // Add the person and the nights staying to the appropriate lists
+        if(!this.original_costs_calculated) // Before original cost calculated
+        {
+            this.nights_staying_list.push(nights_staying);
+            this.name_list.push(name);
+            this.num_guests++;
+        } 
+        else { // After original cost calculated
+            this.nights_staying_list.push(nights_staying);
+            this.name_list.push(name);
+            this.person_shareprice_list_orig.push(0.0); // Adding cost of zero to original shareprice list for the new people
+            this.num_guests += 1;
+        }
+    }
+
+    CalculateOriginalCosts()
+    {
+        this.per_person_cost_each_night_orig = [];
+        this.num_guests_each_night_orig = [];
+        this.person_shareprice_list_orig = [];
+
+        
+        // Calculate per-night cost breakdown
+        for(let i = 0; i < this.num_nights; i++)
+        {
+            let num_guests_tonight = 0;
+            
+            for(let j = 0; j < this.num_guests; j++)
+            {
+                if(this.nights_staying_list[j].includes(i)) // Translate this check
+                {
+                    num_guests_tonight++;
+                }
+            }
+
+            let per_person_cost_tonight = this.per_night_total_cost / num_guests_tonight;
+
+            this.num_guests_each_night_orig.push(num_guests_tonight);
+            this.per_person_cost_each_night_orig.push(per_person_cost_tonight);
+        }
+
+        // Calculate per guest cost breakdown
+        for(let j = 0; j < this.num_guests; j++)
+        {
+            let this_guest_cost = 0;
+
+            for(let i = 0; i < this.num_nights; i++)
+            {
+                if (this.nights_staying_list[j].includes(i)) // Translate this check
+                {
+                    this_guest_cost += this.per_person_cost_each_night_orig[i];
+                }
+            }
+
+            this.person_shareprice_list_orig.push(this_guest_cost)
+        }
+
+        // Check if sums to total price
+        if(Math.abs(this.person_shareprice_list_orig.reduce((partialSum,a) => partialSum+a, 0) - this.total_price) > 1e3 )
+        {
+            // raise Warning('Per person pricing does not sum to total price. Check if there is a night nobody is staying...');
+        }  
+
+        this.original_costs_calculated = true;
+
+        return this.person_shareprice_list_orig;
+    }
+
+    ChangeTotalPrice(new_total_price)
+    {
+        this.total_price = new_total_price;
+        this.per_night_total_cost = this.total_price / this.num_nights;
+    }
+
+    ChangeTotalNights(total_nights)
+    {
+        this.num_nights = total_nights;
+    }
+
+    ChangePersonNights(name, nights_staying)
+    {
+        let index = this.name_list.indexOf(name); // Get index of person with input name   
+        this.nights_staying_list[index] = nights_staying; // Change the list of the nights they're staying
+    }
+
+    RemovePerson(name)
+    {
+        index = this.name_list.indexOf(name); // Get index of person with input name
+        this.nights_staying_list.splice(index,1);
+        this.name_list.splice(index,1);
+        this.num_guests--
+    }
+
+    CalculateRedistribution()
+    {
+        if(!this.original_costs_calculated)
+        {
+            // raise AssertionError("Cannot redistribute until original cost breakdown calculated")
+        }
+
+        this.per_person_cost_each_night_new = []
+        this.num_guests_each_night_new = []
+        this.person_shareprice_list_new = []
+        this.amount_to_send = []
+        
+        // Calculate per-night cost breakdown
+        for(let i = 0; i < this.num_nights; i++)
+        {
+            let num_guests_tonight = 0;
+            
+            for(let j = 0; j < this.num_guests; j++)
+            {
+                if(this.nights_staying_list[j].includes(i)) // Translate this check
+                {
+                    num_guests_tonight++;
+                }
+            }
+
+            let per_person_cost_tonight = this.per_night_total_cost / num_guests_tonight;
+
+            this.num_guests_each_night_new.push(num_guests_tonight);
+            this.per_person_cost_each_night_new.push(per_person_cost_tonight);
+            
+        }
+
+        // Calculate per guest cost breakdown
+        for(let j = 0; j < this.num_guests; j++)
+        {
+            let this_guest_cost = 0;
+
+            for(let i = 0; i < this.num_nights; i++)
+            {
+                if (this.nights_staying_list[j].includes(i)) // Translate this check
+                {
+                    this_guest_cost += this.per_person_cost_each_night_new[i];
+                }
+            }
+
+            this.person_shareprice_list_new.push(this_guest_cost)
+        }
+
+        // Check if sums to total price
+        if(Math.abs(this.person_shareprice_list_new.reduce((partialSum,a) => partialSum+a, 0) - this.total_price) > 1e3 )
+        {
+            // raise Warning('Per person pricing does not sum to total price. Check if there is a night nobody is staying...');
+        }  
+
+        this.original_costs_calculated = true;
+
+        for(let j = 0; j < this.num_guests; j++)
+        {
+            this.amount_to_send.push(this.person_shareprice_list_new[j]- this.person_shareprice_list_orig[j]);
+        }
+    }
+}
+
+// Instance of Stay object
+const stay = new Stay(2717, 7);
+
+
+// Provide info on people staying
+stay.AddPerson("Kevin",  [0,1,2,3,4,5,6])
+stay.AddPerson("Mary",   [0,1,2,3,4,5,6])
+stay.AddPerson("Matthew", [0,1,2,3,4,5,6])
+stay.AddPerson("Amanda", [0,1,2,3,4,5,6])
+stay.AddPerson("Emily",  [0,1,2,3,4,5,6])
+stay.AddPerson("Omkar",  [4,5,6])
+stay.AddPerson("Ansley", [6])
+
+// Calculate original split-up
+stay.CalculateOriginalCosts()
+
+
+// Make post-trip corrections
+stay.ChangePersonNights("Ansley", [4,5,6])
+
+
+// Calculate Redistribution
+stay.CalculateRedistribution()
---
+++ docs/web/JavaScript/siteLogic.js
@@ -0,0 +1,191 @@
+// Wait until the window finishes loaded before executing any script
+window.onload = function() {
+
+    var totalCost, totalNights;
+    let orgPersons = [];
+    let orgNights = [];
+    var orgCosts = [];
+
+    var recal = 1;
+    var recalculating = false;
+    
+    // Initialize the activityNumber
+    var activityNumber = 1;
+
+    // Select the add_activity button
+    var addButton = document.getElementById("add_activity");
+
+    // Select the calculate button
+    var calcButton = document.getElementById("calculate");
+
+    // Select the table element
+    var calcTableJS = document.getElementById("calcTable");
+    var recalTableJS = document.getElementById("recalTable");
+    var redisTableJS = document.getElementById("redisTable");
+    var calcResultTableJS = document.getElementById("calcResultTable");
+
+    // Select the table element
+    var resultTable = document.getElementById("resultlist");
+
+    const stay = new Stay(document.getElementById("totalCost").value, document.getElementById("totalNights").value);
+
+    // Attach handler to the button click event
+    addButton.onclick = function() {
+
+    // Add a new row to the table using the correct activityNumber
+
+        if (recalculating){
+        recalTableJS.insertAdjacentHTML("beforeend", '<tr><td><label>Person</label><br/> \
+            <input type="text" name="reperson' + recal + '" id="AddReperson' + recal + '"  value="" placeholder="Name"> \
+            <input type="text" name="renights' + recal + '" id="AddRenights' + recal + '" value="" placeholder="Nights"></td></tr>');
+            recal++;
+        }
+        else{
+        calcTableJS.insertAdjacentHTML("beforeend", '<tr><td><label>Person ' + activityNumber + ': </label><br/> \
+            <input type="text" name="person' + activityNumber + '" id="person' + activityNumber + '" class="required" placeholder="Name"> \
+            <input type="text" id="nights' + activityNumber + '" name="nights' + activityNumber + '" value="" placeholder="Nights"></td></tr>');
+            activityNumber += 1;
+        }        
+    }
+
+    document.getElementById("calculate").addEventListener("click", function() {
+
+
+        orgPersons = [];
+        orgNights = [];
+        totalCost = document.getElementById("totalCost").value;
+        totalNights = document.getElementById("totalNights").value;
+
+        const stay = new Stay(totalCost, totalNights);
+
+        var persons, nights, index;
+        
+        persons = document.querySelectorAll('[id^="person"]');
+        var personsArr = Array.from(persons);
+
+        nights = document.querySelectorAll('[id^="nights"]');
+        var nightsArr = Array.from(nights);
+
+        for (index = 0; index < personsArr.length; index++) {
+            var per, night;
+
+            per = personsArr[index].value;
+            orgPersons.push(per);
+            //index++;
+            night = nightsArr[index].value;
+            orgNights.push(night);
+
+            stay.AddPerson(per,  Array.from(String(night), Number));
+
+        }
+
+        orgCosts = stay.CalculateOriginalCosts();
+
+        calcResultTableJS.innerHTML = '';
+        for (index = 0; index < orgCosts.length; index++) {
+            calcResultTableJS.insertAdjacentHTML("beforeend", '<tr><td><label>' + orgPersons[index] + ': $</label><label>' + orgCosts[index].toFixed(2) + '</label><br/></td></tr>');
+        }
+
+        document.getElementById("recalculate").disabled = false;
+
+    }, false);
+
+
+    document.getElementById("recalculate").addEventListener("click", function() {
+
+        var persons, nights, index;
+
+        persons = document.querySelectorAll('[id^="person"]');
+        nights = document.querySelectorAll('[id^="nights"]');
+
+        for(index = 1; index <= persons.length; index++){
+            document.getElementById("person" + index).disabled = true;
+            document.getElementById("nights" + index).disabled = true;
+        }
+        
+        document.getElementById("totalCost").disabled = true;
+        document.getElementById("totalNights").disabled = true;
+
+        recalculating = true;
+        recal = 1;
+
+        recalTableJS.insertAdjacentHTML("beforeend", '<tr><td><label><b>Modifications:</b></label></td></tr>');
+
+        for (index = 0; index < orgPersons.length; index++) {
+            recalTableJS.insertAdjacentHTML("beforeend", '<tr><td><label>' + orgPersons[index] + ' </label><br/> \
+            <input type="text" name="reperson' + recal + '" id="reperson' + recal + '"  value="' + orgPersons[index] + '"> \
+            <input type="text" name="renights' + recal + '" id="renights' + recal + '" value="' + orgNights[index] + '" ></td></tr>');
+            recal++;
+        }
+
+        document.getElementById("calculate").disabled = true;
+        document.getElementById("recalculate").disabled = true;
+        document.getElementById("redistribute").disabled = false;
+        document.getElementById("add_activity").disabled = false;
+
+
+    }, false);
+
+    document.getElementById("redistribute").addEventListener("click", function() {
+
+        var index;
+        let repersons = [];
+        let renights = [];
+
+        const stay = new Stay(totalCost, totalNights);
+
+        for (index = 0; index < orgPersons.length; index++) {
+            stay.AddPerson(orgPersons[index],  Array.from(String(orgNights[index]), Number));
+        }
+
+        stay.CalculateOriginalCosts();
+        
+        repersons = document.querySelectorAll('[id^="reperson"]');
+        var repersonsArr = Array.from(repersons);
+        renights = document.querySelectorAll('[id^="renights"]');
+        var renightsArr = Array.from(renights);
+
+        AddRepersons = document.querySelectorAll('[id^="AddReperson"]');
+        var AddRepersonsArr = Array.from(AddRepersons);
+        AddRenights = document.querySelectorAll('[id^="AddRenights"]');
+        var AddRenightsArr = Array.from(AddRenights);
+
+        for (index=0; index < repersonsArr.length; index++){
+
+            if (renightsArr[index].value != orgNights[index]){
+                stay.ChangePersonNights(repersonsArr[index].value, Array.from(String(renightsArr[index].value), Number))
+            }
+
+        }
+
+        for (index = 0; index < AddRepersons.length; index++) {
+            stay.AddPerson(AddRepersonsArr[index].value, Array.from(String(AddRenightsArr[index].value), Number))
+        }
+
+        stay.CalculateRedistribution();
+
+
+        redisTableJS.insertAdjacentHTML("beforeend", '<tr><td><label><b>Money Redistribution:</b></label><br/></td></tr>');
+
+        //print(" - Guest {} is staying {} nights and owes ${:.2f}.".format(self.name_list[j],len(self.nights_staying_list[j]),self.person_shareprice_list_new[j]))
+
+        redisTableJS.innerHTML = '';
+        for (index = 0; index < stay.num_guests; index++) {
+            redisTableJS.insertAdjacentHTML("beforeend", '<tr><td><label><b>' + stay.name_list[index] + '</b> is staying ' + stay.nights_staying_list[index].length + ' nights and <b>owes</b>: $</label><label><b>' + stay.person_shareprice_list_new[index].toFixed(2) + '</b></label><br/></td></tr>');
+        }
+
+        redisTableJS.insertAdjacentHTML("beforeend", '<tr><td><br/></td></tr>');
+
+        //print(" - Guest {} should {} ${:.2f}.".format(self.name_list[j], "send" if self.amount_to_send[j]>0 else "receive", self.amount_to_send[j]))
+        
+        for (index = 0; index < stay.num_guests; index++) {
+            redisTableJS.insertAdjacentHTML("beforeend", '<tr><td><label><b>' + stay.name_list[index] + '</b> should ' + ((stay.amount_to_send[index] > 0) ? '<b>send</b>' : '<b>receive</b>') + ': $</label><label><b>' + Math.abs(stay.amount_to_send[index]).toFixed(2) + '</b></label><br/></td></tr>');
+        }
+
+        //document.getElementById("add_activity").disabled = true;
+        //document.getElementById("redistribute").disabled = true;
+
+
+    }, false);
+
+}
